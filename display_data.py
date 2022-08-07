4b49cb9d07ffb03f74b226f0a815b39e74fe2ca2
gisma <me@billions.sh>
2022-08-07 18:10:50 -0300

    html site will now reload the .csv file when the browser is reloaded and display the latest data

--- display_data.py
+++ display_data.py
@@ -2,24 +2,32 @@ from dash import Dash, dash_table, dcc, html
 import pandas as pd
 import plotly.graph_objs as go
 
-df = pd.read_csv('data.csv', index_col=False)
-
-fig = go.Figure(data=[go.Scatter(x=df.date, y=df.net_value)])
-
 app = Dash(__name__)
 app.title = "Curve staked ETH/stETH pool monitor"
-app.layout = html.Div(children=[
 
-    html.H1(children='Curve staked ETH/stETH pool monitor'),
 
-    html.Div(children='''
+def serve_layout():
+    # Load the data.
+    df = pd.read_csv('data.csv', index_col=False)
+    fig = go.Figure(data=[go.Scatter(x=df.date, y=df.net_value)])
+
+    # Render the layout.
+
+    return html.Div(children=[
+
+        html.H1(children='Curve staked ETH/stETH pool monitor'),
+
+        html.Div(children='''
         x-axis: time, y-axis net worth including lido interest.
     '''),
 
-    dcc.Graph(figure=fig),
+        dcc.Graph(figure=fig),
 
-    dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
-])
+        dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+    ])
+
+
+app.layout = serve_layout
 
 if __name__ == '__main__':
     app.run_server(host="0.0.0.0", port="80")
